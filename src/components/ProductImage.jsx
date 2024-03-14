4a3b3187dee43c29778505ca165d777d654fab6f
Sahadev <sahadevbh8@gmail.com>
2024-03-14 19:26:04 +0530

    shifted the images from /src/assets to /public

--- src/components/ProductImage.jsx
+++ src/components/ProductImage.jsx
@@ -2,12 +2,12 @@ function ProductImage() {
   return (
     <div>
       <img
-        src="src/assets/image-product-desktop.jpg"
+        src="/image-product-desktop.jpg"
         alt=""
         className="w-72 rounded-l-xl max-[575px]:hidden"
       />
       <img
-        src="src/assets/image-product-mobile.jpg"
+        src="/image-product-mobile.jpg"
         alt=""
         className="w-72 rounded-t-xl hidden max-[575px]:block max-[575px]:w-full"
       />
